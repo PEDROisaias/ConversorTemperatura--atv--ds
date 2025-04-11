22c3544f74223e504b6ebd5c9e260d3917b3a13f
PEDROisaias <163759050+PEDROisaias@users.noreply.github.com>
2025-04-11 14:57:41 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        ConversorTemperatura conversor = new ConversorTemperatura();
+
+        double celsius = 30;
+        double resultadoF = conversor.converter(celsius);
+        System.out.println(celsius + "°C em Fahrenheit: " + resultadoF);
+
+        double fahrenheit = 86;
+        double resultadoC = conversor.converterFahrenheitParaCelsius(fahrenheit);
+        System.out.println(fahrenheit + "°F em Celsius: " + resultadoC);
+
+        int resultadoAjustado = conversor.converter(celsius, 2, true);
+        System.out.println(celsius + "°C em Fahrenheit com ajuste de 2 e arredondamento: " + resultadoAjustado);
+    }
+}
